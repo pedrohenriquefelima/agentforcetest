68f404ebf34abcc3cad2e54034a69577605ee873
pedrohenriquefelima <pedrohenriquefelima@gmail.com>
2025-06-12 17:43:29 +0100

    apex-field-should-error

--- force-app/main/default/lwc/testCaculator/testCaculator.js
+++ force-app/main/default/lwc/testCaculator/testCaculator.js
@@ -1,5 +1,4 @@
 import { LightningElement } from 'lwc';
-import EMAIL_FIELD from "@salesforce/schema/Account.my_useless_field__c";
 
 export default class TestCaculator extends LightningElement {
     
