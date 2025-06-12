5164f1c7a21e9f4e3ccb58515741a9fedbfe306a
pedrohenriquefelima <pedrohenriquefelima@gmail.com>
2025-06-12 17:39:22 +0100

    importing-field

--- force-app/main/default/lwc/testCaculator/testCaculator.js
+++ force-app/main/default/lwc/testCaculator/testCaculator.js
@@ -1,4 +1,5 @@
 import { LightningElement } from 'lwc';
+import EMAIL_FIELD from "@salesforce/schema/Account.my_useless_field__c";
 
 export default class TestCaculator extends LightningElement {
     
